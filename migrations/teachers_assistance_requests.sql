f49a8d4902df3caabb38fdcea592a251716a3b85
graemenickerson <graeme.nickerson@gmail.com>
2019-10-15 20:05:55 +0000

    Adds teachers and assistance requests to database.

---
+++ migrations/teachers_assistance_requests.sql
@@ -0,0 +1,23 @@
+-- teachers_assistance_requests.sql
+-- Graeme Nickerson
+-- October 2019
+
+CREATE TABLE teachers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  is_active BOOLEAN DEFAULT TRUE,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE assistance_requests (
+  id SERIAL PRIMARY KEY NOT NULL,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  created_at TIMESTAMP,
+  started_at TIMESTAMP,
+  completed_at TIMESTAMP,
+  student_feedback TEXT,
+  teacher_feedback TEXT
+);
