0b8555d58a88ab6e57ed29641714f87d631be2de
graemenickerson <graeme.nickerson@gmail.com>
2019-10-17 00:13:59 +0000

    Adds bootcamp_app to BootcampX.

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,25 @@
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+  FROM assistance_requests
+  JOIN students ON students.id = student_id
+  JOIN teachers ON teachers.id = teacher_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name = '${process.argv[2]}'
+  ORDER BY teacher;
+`)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.teacher} helped the ${user.cohort} cohort`);
+    })
+  })
+  .catch(err => console.error('query error', err.stack));
